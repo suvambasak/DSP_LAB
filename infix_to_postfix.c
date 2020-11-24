a25d500d24805cede3725275075c58cda373df32
suvambasak <suvambasak@gmail.com>
2020-11-24 17:59:57 +0530

    in to po

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -1,11 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
+#include<ctype.h> 
 #define SIZE 100
 
 char op_stack[SIZE];
 int num_stack[SIZE];
 int op_top=-1,num_top=-1;
+
+
+typedef struct node{
+	int flg;
+	int num;
+	struct node *next;
+}node;
+
+node *HEAD=NULL;
+
+
+int insert(int value, int flag){
+	node *p = (node*)malloc(sizeof(node));
+	p->next=NULL;
+	p->flg = flag;
+	p->num = value;
+
+	if (HEAD==NULL){
+		HEAD=p;
+		p=NULL;
+	}else{
+		node *temp = HEAD;
+		while(temp->next != NULL)
+			temp=temp->next;
+		
+		temp->next=p;
+		p=NULL;
+		temp=NULL;
+	}
+	return 1;
+}
+
+
+int display(){
+	if(HEAD==NULL){
+		printf("\n\tLIST IS EMPTY!!");
+		return 0;
+	}else{
+		node *temp=HEAD;
+		printf("\nDisplay: ");
+		while(temp!=NULL){
+			if (temp->flg)
+				printf("%c ",temp->num);
+			else
+				printf("%d ",temp->num);
+			temp=temp->next;
+		}
+		return 1;
+	}
+}
 
 int is_numstk_full(){
 	if(num_top==SIZE-1)
@@ -99,57 +150,29 @@ int is_operator(char ch){
     }
 }
 
+void infix_to_postfix(char* infix){
+	char c;
+	for (int i=0;i<strlen(infix);i++){
+		// c = infix[i];
+		if(isdigit(infix[i])){
+			int d = infix[i]-'0';
+			while (isdigit(infix[i+1])){
+				d*=10;
+				d+=infix[i+1]-'0';
+				i++;
+			}
+			insert(d,0);
+		}else{
+			insert(infix[i],1);
+		}
+			
+	}
+}
+
 int main(){
-    char infix[SIZE];
-
-    // scanf("%s",infix);
-    // printf("%s",infix);
-    // printf("%ld",strlen(infix));
-
-    // printf("%d",get_precedence('^'));
-    // printf("%d",is_operator('*'));
-
-    // push_opstk('+');
-    // push_opstk('-');
-    // push_opstk('*');
-    // push_opstk('^');
-    // push_opstk('/');
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // push_opstk('+');
-    // push_opstk('-');
-    // push_opstk('*');
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-    // printf("%c ",pop_opstk());
-
-    // push_numstk(10);
-    // push_numstk(20);
-    // push_numstk(30);
-    // push_numstk(40);
-    // push_numstk(50);
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // push_numstk(30);
-    // push_numstk(40);
-    // push_numstk(50);
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
-    // printf("%d ",pop_numstk());
+    char infix[SIZE] = "10+(3*4-(4/2^2)*5)*3";
+	infix_to_postfix(infix);
+	display();
 
     return 0;
 }
