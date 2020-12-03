08c5078999954faf9d450b038731a70db651b4f9
suvambasak <suvambasak2008@gmail.com>
2020-12-03 19:18:23 +0530

    Q1 Q2

---
+++ week_3/circular_prime.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Function to get number of digit.
+int numberofdigits(int num)
+{
+    int counter = 0;
+    while ((++counter) && (num /= 10))
+        ;
+    return counter;
+}
+
+// Function to check prime.
+int isprime(int num)
+{
+    int i;
+    for (i = 2; i <= (num / 2); i++)
+        if ((num % i) == 0)
+            return 0;
+    return 1;
+}
+
+// Function for number rotation.
+int rotate(int num)
+{
+    return ((num % 10) * pow(10, numberofdigits(num) - 1) + (num / 10));
+}
+
+// Function for rotate and check each number is prime or not.
+int circular_parmut(int num)
+{
+    // Invalid input.
+    if (num <= 0)
+        return -1;
+    if (num == 1)
+        return 0;
+
+    int number_of_digits = numberofdigits(num);
+    int i;
+
+    // Checking each rotation.
+    for (i = 1; i <= number_of_digits; i++)
+    {
+        if (isprime(num))
+        {
+            num = rotate(num);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Main function.
+int main()
+{
+    int number;
+
+    printf("\nENTER A NUMBER : ");
+    scanf("%d", &number);
+
+    printf("%d", circular_parmut(number));
+
+    return 0;
+}
+
+// #######################################################################################################
+// A number is circular prime if the number and all its cyclic permutations are prime.
+
+// For example:
+
+// The number 3779 is circular prime because all the cyclic permutations
+
+// 3779, 9377, 7937, 7793 are prime numbers.
+
+// Where as the number 3877 is not circular prime because one of its cyclic permutation 7738 is not a prime number.
+
+// Input/Output Specification:
+
+//  - Any input <= 0 to be treated as invalid input. Any input > 0 to be treated as valid input
+
+//  - If the number <= 0, your program should print -1
+
+//  - If the number is circular prime, your program should print 1
+
+//  - If the number is not circular prime, your program should print 0
+
+// Sample Input/Output:
+
+// Input:
+
+//  -100
+
+//  Output:
+
+//  -1
+
+// Input:
+
+//  0
+
+//  Output:
+
+//  -1
+
+// Input:
+
+//  3779
+
+//  Output:
+
+//  1
+
+//  Input:
+
+//  3877
+
+//  Output:
+
+//  0
+
+//  ######################################################################################################
---
+++ week_3/selection.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function.
+int main()
+{
+
+    int arr[100];
+    int len, i, j, minpos, temp;
+
+    // Size input.
+    printf("ENTER THE SIZE: ");
+    scanf("%d", &len);
+
+    // Input Array.
+    printf("ENTER THE %d ELEMENT(S): ", len);
+    for (i = 0; i < len; i++)
+        scanf("%d", &arr[i]);
+
+    // Selection sort.
+    for (i = 0; i < len; i++)
+    {
+        minpos = i;
+        for (j = i + 1; j < len; j++)
+            if (arr[minpos] > arr[j])
+                minpos = j;
+
+        temp = arr[minpos];
+        arr[minpos] = arr[i];
+        arr[i] = temp;
+    }
+
+    // Show.
+    printf("SORTED: ");
+    for (i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+
+    return 0;
+}
+
+// ####################################################################
+// input:
+// first line : number of elements : 10
+// next unsorted array :10, 14, 28, 11, 7, 16, 30, 50, 25, 18
+// output: 7, 10, 11, 14, 16, 18, 25, 28, 30, 50
+
+// input =
+// 10
+// 10 14 28 11 7 16 30 50 25 18
+
+// output =  10 11 14 16 18 25 28 30 50
+// ####################################################################
